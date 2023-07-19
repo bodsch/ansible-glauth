a7b04b7a972b57123fb77608bbe0a9b7f9c7d13e
Bodo Schulz <bodo@boone-schulz.de>
2023-07-19 06:17:23 +0200

    fix molecule tests

--- molecule/database-config/tests/test_default.py
+++ molecule/database-config/tests/test_default.py
@@ -45,7 +45,7 @@ def read_ansible_yaml(file_name, role_name):
     read_file = None
 
     for e in ["yml", "yaml"]:
-        test_file = "{}.{}".format(file_name, e)
+        test_file = f"{file_name}.{e}"
         if os.path.isfile(test_file):
             read_file = test_file
             break
@@ -110,7 +110,7 @@ def local_facts(host):
 @pytest.mark.parametrize("dirs", [
     "/etc/glauth",
     "/var/lib/glauth",
-    "/usr/local/bin/glauth",
+    "/usr/local/opt/glauth",
 ])
 def test_directories(host, dirs):
     d = host.file(dirs)
